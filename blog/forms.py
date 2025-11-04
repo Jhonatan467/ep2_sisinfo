9d99703c8981acc8442112ed896e4d56d26d5e19
Jhonatan Santos <jhonatanribeirosts@gmail.com>
2025-11-04 00:10:58 -0300

    feat: switch views to use forms

---
+++ blog/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+from django.utils import timezone
+
+from .models import Post
+
+
+class PostForm(forms.ModelForm):
+    created_at = forms.DateTimeField(
+        label="Data de publicação",
+        widget=forms.DateTimeInput(
+            attrs={"class": "form-control", "type": "datetime-local"},
+            format="%Y-%m-%dT%H:%M",
+        ),
+        input_formats=["%Y-%m-%dT%H:%M"],
+    )
+
+    class Meta:
+        model = Post
+        fields = ["title", "content", "created_at"]
+        widgets = {
+            "title": forms.TextInput(attrs={"class": "form-control"}),
+            "content": forms.Textarea(attrs={"class": "form-control", "rows": 8}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        current_tz = timezone.get_current_timezone()
+        if self.instance and self.instance.pk and "created_at" not in self.initial:
+            self.initial["created_at"] = timezone.localtime(self.instance.created_at, current_tz).strftime("%Y-%m-%dT%H:%M")
+        elif "created_at" not in self.initial:
+            self.initial["created_at"] = timezone.localtime(timezone.now(), current_tz).strftime("%Y-%m-%dT%H:%M")
+
+    def clean_created_at(self):
+        created_at = self.cleaned_data["created_at"]
+        if timezone.is_naive(created_at):
+            return timezone.make_aware(created_at, timezone.get_current_timezone())
+        return created_at
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
-from django.utils import timezone
 
+from .forms import PostForm
 from .models import Post
 
 
@@ -16,33 +16,20 @@ def post_detail(request, pk):
 
 
 def post_create(request):
-	if request.method == "POST":
-		title = request.POST.get("title", "").strip()
-		content = request.POST.get("content", "").strip()
-		created_at_raw = request.POST.get("created_at")
+	form = PostForm(request.POST or None)
 
-		if created_at_raw:
-			try:
-				created_at = timezone.datetime.fromisoformat(created_at_raw)
-				if timezone.is_naive(created_at):
-					created_at = timezone.make_aware(created_at, timezone.get_current_timezone())
-			except ValueError:
-				created_at = timezone.now()
-		else:
-			created_at = timezone.now()
-
-		post = Post.objects.create(title=title, content=content, created_at=created_at)
+	if request.method == "POST" and form.is_valid():
+		post = form.save()
 		return redirect("post-detail", pk=post.pk)
 
-	default_created_at = timezone.localtime(timezone.now()).strftime("%Y-%m-%dT%H:%M")
 	return render(
 		request,
 		"blog/post_form.html",
 		{
+			"form": form,
+			"post": None,
 			"form_action": reverse("post-create"),
 			"submit_label": "Salvar",
-			"post": None,
-			"default_created_at": default_created_at,
 		},
 	)
 
@@ -50,32 +37,20 @@ def post_create(request):
 def post_update(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 
-	if request.method == "POST":
-		post.title = request.POST.get("title", "").strip()
-		post.content = request.POST.get("content", "").strip()
-		created_at_raw = request.POST.get("created_at")
+	form = PostForm(request.POST or None, instance=post)
 
-		if created_at_raw:
-			try:
-				created_at = timezone.datetime.fromisoformat(created_at_raw)
-				if timezone.is_naive(created_at):
-					created_at = timezone.make_aware(created_at, timezone.get_current_timezone())
-				post.created_at = created_at
-			except ValueError:
-				pass
-
-		post.save()
+	if request.method == "POST" and form.is_valid():
+		form.save()
 		return redirect("post-detail", pk=post.pk)
 
-	default_created_at = timezone.localtime(post.created_at).strftime("%Y-%m-%dT%H:%M")
 	return render(
 		request,
 		"blog/post_form.html",
 		{
+			"form": form,
+			"post": post,
 			"form_action": reverse("post-update", kwargs={"pk": post.pk}),
 			"submit_label": "Atualizar",
-			"post": post,
-			"default_created_at": default_created_at,
 		},
 	)
 
