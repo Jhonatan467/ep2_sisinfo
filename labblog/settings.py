dde26fca45601a6246a81f87b78694ff2d896373
Jhonatan Santos <jhonatanribeirosts@gmail.com>
2025-11-25 22:09:39 -0300

    Configura ALLOWED_HOSTS via variável de ambiente

--- labblog/settings.py
+++ labblog/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-)ogxiy%gv9_1jsqpbtj9nav!*fy3(i&%eos+pg0bz6-wd76-s7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 
 # Application definition
